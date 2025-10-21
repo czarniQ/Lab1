84eb5ecd1dfbf632cca5df80629a341b91d8fdbd
czarniQ <kubaczarnik1@gmail.com>
2025-10-21 19:30:35 +0200

    poprawka

--- zad1b.py
+++ zad1b.py
@@ -1,5 +1,5 @@
-print(int(3.0)) #int konwertuje wartość na liczbę całkowitą
-print(float(3)) #float konwertuje wartość na liczbę zmiennoprzecinkową
-print(float("3")) #float konwertuje napis "3" na liczbę zmiennoprzecinkową
-print(str(12.4)) #str konwertuje liczbę na ciąg znaków
+print(int(3.0)) #int zmienia wartość na liczbę całkowitą
+print(float(3)) #float zmieniawartość na liczbę z przecinkami
+print(float("3")) #float zmienia napis "3" na liczbę z przecinkami
+print(str(12.4)) #str zmienia liczbę na ciąg znaków
 print(bool(0)) #bool to prawda i fałsz (0 to fałsz)
